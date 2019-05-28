c6fb50733050c1ba1960b816c2303d1603c20764
Shabdan <shabdan.abjaparov.official@gmail.com>
2019-05-28 20:54:24 +0600

    Next step is to finish orderController with two separate methods: one gets actual order description, the other one is only to add items to the order

--- src/main/java/com/example/demo/bootstrap/MainBootstrap.java
+++ src/main/java/com/example/demo/bootstrap/MainBootstrap.java
@@ -61,8 +61,6 @@ public class MainBootstrap implements CommandLineRunner {
         itemList.add(new Item("Juice","1L",100.0,50.0));
         itemList.add(new Item("Burger","2pcs",300.0,150.0));
         //(User orderedBy, Courier courier, Double moneyAmount, String market)
-        orderService.createOrder(new OrderModel(
-                new Order(user1,null,900.0,"GLOBUS"),itemList
-        ));
+        orderService.createOrder(new Order(user1,null,900.0,"GLOBUS"));
     }
 }
